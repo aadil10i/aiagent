8f9f6a2b69a443a3099e920113f97db43a111513
aadil10i <aadil3003@outlook.com>
2025-06-19 21:19:37 +0400

    token count

--- main.py
+++ main.py
@@ -10,5 +10,7 @@ response = client.models.generate_content(
     model="gemini-2.0-flash-001",
     contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
 )
+p_token = response.usage_metadata.prompt_token_count
+r_token = response.usage_metadata.candidates_token_count
 
-print(response.text)
+print(f"{response.text} \n Prompt Tokens: {p_token} \n Response Tokens: {r_token}")

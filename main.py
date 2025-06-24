ecbc9d0d10d08c2cc021a98567b9b0c38ab3e964
aadil10i <aadil3003@outlook.com>
2025-06-24 09:31:59 +0400

    input and response sys

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,10 +7,13 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-)
+if len(sys.argv) < 2:
+    print("error no input provided")
+
+input = sys.argv[1]
+
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=input)
+
 p_token = response.usage_metadata.prompt_token_count
 r_token = response.usage_metadata.candidates_token_count
 

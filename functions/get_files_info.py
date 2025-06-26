895d2e1eff066ba3ff849083e61041ea1088a41b
aadil10i <aadil3003@outlook.com>
2025-06-26 21:53:18 +0400

    file info

---
+++ functions/get_files_info.py
@@ -0,0 +1,33 @@
+import os
+from collections import defaultdict
+
+
+def get_files_info(working_directory, directory=None):
+    if directory is None:
+        path_to_check = working_directory
+    else:
+        path_to_check = os.path.join(working_directory, directory)
+
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_path_to_check = os.path.abspath(path_to_check)
+
+    if not abs_path_to_check.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if not os.path.isdir(path_to_check):
+        return f'Error: "{directory}" is not a directory'
+
+    file_contents = os.listdir(path_to_check)
+
+    for i in range(len(file_contents)):
+        file_name = file_contents[i]
+        file_size = os.path.getsize(os.path.join(path_to_check, file_contents[i]))
+        is_dir = os.path.isdir(os.path.join(path_to_check, file_contents[i]))
+
+        print(f"- {file_name}: file_size={file_size} bytes, is_dir={is_dir}")
+
+
+# os.listdir(): List the contents of a directory
+# os.path.getsize(): Get the size of a file
+# os.path.isfile(): Check if a path is a file
+# .join(): Join a list of strings together with a separator

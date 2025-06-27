38d9495dc27be91428f4fe850c1275be916ba1ce
aadil10i <aadil3003@outlook.com>
2025-06-27 17:58:33 +0400

    test file

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-if len(sys.argv) < 2:
-    print("error no input provided")
-    sys.exit(1)
+user_input = sys.argv[1]
+messages = [types.Content(role="user", parts=[types.Part(text=user_input)])]
 
-input = sys.argv[1]
-
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=input)
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001", contents=messages
+)
 
 p_token = response.usage_metadata.prompt_token_count
 r_token = response.usage_metadata.candidates_token_count
+
+if len(sys.argv) < 2:
+    print("error no input provided")
+    sys.exit(1)
 
 print(f"{response.text} \n Prompt Tokens: {p_token} \n Response Tokens: {r_token}")
